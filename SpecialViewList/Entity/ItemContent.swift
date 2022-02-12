a2a18291a9ce74fffad212a56cb2f59d777f9580
yosei-yamagishi <yamagishi@radiotalk.jp>
2022-02-12 14:12:35 +0900

    Entityを追加

---
+++ SpecialViewList/Entity/ItemContent.swift
@@ -0,0 +1,18 @@
+struct ItemContent {
+    let name: String
+}
+
+extension ItemContent {
+    static let listContents: [ItemContent] = [
+        ItemContent(name: "アイテム１"),
+        ItemContent(name: "アイテム２２"),
+        ItemContent(name: "アイテム３３３"),
+        ItemContent(name: "アイテム４４４４"),
+        ItemContent(name: "アイテム５５５５５"),
+        ItemContent(name: "アイテム６６６６６６"),
+        ItemContent(name: "アイテム７７７７７７７"),
+        ItemContent(name: "アイテム８８８８８８８８"),
+        ItemContent(name: "アイテム９９９９９９９９９"),
+    ]
+    
+}
