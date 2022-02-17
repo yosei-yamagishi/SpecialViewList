68877a9d0ba7359800fb5b48b5dffc6244afc4fc
yosei-yamagishi <yamagishi@radiotalk.jp>
2022-02-18 08:26:44 +0900

    表示するためのVCを追加

---
+++ SpecialViewList/Component/AutoFlowedTitleView/AutoFlowedTitleViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class AutoFlowedTitleViewController: UIViewController {
+
+    @IBOutlet weak var autoFlowedTitleView: AutoFlowedTitleView!
+    private let viewModel: AutoFlowedTitleViewModel
+    
+    init(viewModel: AutoFlowedTitleViewModel = AutoFlowedTitleViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = viewModel.specialViewListItem.title
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        autoFlowedTitleView.initTitle(title: viewModel.title)
+    }
+}
