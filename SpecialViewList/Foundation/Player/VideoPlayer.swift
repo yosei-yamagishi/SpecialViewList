9b9056f0def582d17601fb6f8d23e1712f8205f2
yosei-yamagishi <yamagishi@radiotalk.jp>
2022-06-16 15:01:25 +0900

    動画Playerの追加

---
+++ SpecialViewList/Foundation/Player/VideoPlayer.swift
@@ -0,0 +1,37 @@
+import AVFoundation
+
+protocol VideoPlayerProtocol {
+    var player: AVPlayer? { get }
+}
+
+protocol VideoPlayerControlProtocol {
+    var isPlaying: Bool { get }
+    func play(isMuted: Bool)
+    func pauseAndInit()
+    func mute(isMuted: Bool)
+    func prepare(url: URL)
+}
+
+class VideoPlayer: VideoPlayerProtocol, VideoPlayerControlProtocol {
+    var player: AVPlayer?
+    var isPlaying: Bool { player?.isPlaying ?? false }
+
+    func prepare(url: URL) {
+        let item = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: item)
+    }
+    
+    func play(isMuted: Bool) {
+        mute(isMuted: isMuted )
+        player?.play()
+    }
+    
+    func pauseAndInit() {
+        player?.pause()
+        player = nil
+    }
+    
+    func mute(isMuted: Bool) {
+        player?.volume = isMuted ? 0 : 1
+    }
+}
