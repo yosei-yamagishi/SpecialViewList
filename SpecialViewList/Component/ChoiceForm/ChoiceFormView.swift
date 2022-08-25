a876e9f5ca1fddac22ed5a2cb67dc71dcab243cd
yosei-yamagishi <yamagishi@radiotalk.jp>
2022-08-25 17:56:34 +0900

    新しいCustomViewの追加

---
+++ SpecialViewList/Component/ChoiceForm/ChoiceFormView.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ChoiceFormView: UIView, NibOwnerLoadable {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNibContent()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadNibContent()
+    }
+}
