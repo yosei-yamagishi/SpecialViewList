72096a835685be3b14a778622302324d22d9e57b
yosei-yamagishi <yamagishi@radiotalk.jp>
2022-08-25 17:56:08 +0900

    新規画面の追加

---
+++ SpecialViewList/Component/ChoiceForm/ChoiceFormViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class ChoiceFormViewController: UIViewController {
+
+    private let viewModel: ChoiceFormViewModel
+    
+    init(viewModel: ChoiceFormViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
