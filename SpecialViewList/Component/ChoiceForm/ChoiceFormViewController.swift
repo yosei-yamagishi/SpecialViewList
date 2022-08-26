730f1c3106527fd0459aae7cafcef44db555a30d
yosei-yamagishi <yamagishi@radiotalk.jp>
2022-08-27 00:24:25 +0900

    選択肢を表示するVCを追加

--- SpecialViewList/Component/ChoiceForm/ChoiceFormViewController.swift
+++ SpecialViewList/Component/ChoiceForm/ChoiceFormViewController.swift
@@ -1,7 +1,14 @@
 import UIKit
+import Combine
 
 class ChoiceFormViewController: UIViewController {
-
+    @IBOutlet weak var choiceFormView: ChoiceFormView! {
+        didSet {
+            choiceFormView.delegate = self
+        }
+    }
+    
+    private var cancellables = Set<AnyCancellable>()
     private let viewModel: ChoiceFormViewModel
     
     init(viewModel: ChoiceFormViewModel) {
@@ -16,5 +23,35 @@ class ChoiceFormViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindViewModel()
+    }
+    
+    private func bindViewModel() {
+        viewModel.$choices.receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] choices in
+                self?.choiceFormView.updateChoices(choices: choices)
+            })
+            .store(in: &cancellables)
+    }
+}
+
+extension ChoiceFormViewController: ChoiceFormViewDelegate {
+    func editingChoice(position: Int, text: String) {
+        viewModel.updateChoice(
+            position: position,
+            choice: text
+        )
+    }
+    
+    func didTapAddChoice() {
+        viewModel.addChoice()
+    }
+    
+    func didTapRemoveChoice(position: Int) {
+        viewModel.removeChoice(position: position)
+    }
+    
+    func didBeginEditing() {
+        
     }
 }
