986340875c0ff9cbd83b946b890d405b6d928411
yosei-yamagishi <yamagishi@radiotalk.jp>
2022-08-27 00:25:01 +0900

    選択肢を表示するVMを追加

--- SpecialViewList/Component/ChoiceForm/ChoiceFormViewModel.swift
+++ SpecialViewList/Component/ChoiceForm/ChoiceFormViewModel.swift
@@ -1,1 +1,21 @@
-class ChoiceFormViewModel {}
+import Combine
+
+class ChoiceFormViewModel {
+    @Published var choices: [String] = [""]
+    
+    func updateChoice(position: Int, choice: String) {
+        let index = position - 1
+        choices[index] = choice
+    }
+    
+    func addChoice() {
+        choices.append("")
+    }
+    
+    func removeChoice(position: Int) {
+        if choices.count > 1 {
+            let index = position - 1
+            choices.remove(at: index)
+        }
+    }
+}
