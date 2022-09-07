d7a7c7c2abb9a121a439de21cf85b0ed47f3eca8
yosei <22518469+yosei-yamagishi@users.noreply.github.com>
2022-09-07 23:20:30 +0900

    TableViewの不要なExtensionを追加

--- SpecialViewList/Extenstion/UITableViewExtension.swift
+++ SpecialViewList/Extenstion/UITableViewExtension.swift
@@ -1,15 +1,6 @@
 import UIKit
 
 extension UITableView {
-    func register<T: UITableViewCell>(cellType: T.Type) {
-        let className = cellType.className
-        register(cellType, forCellReuseIdentifier: className)
-    }
-
-    func register<T: UITableViewCell>(cellTypes: [T.Type]) {
-        cellTypes.forEach { register(cellType: $0) }
-    }
-
     func registerNib<T: UITableViewCell>(cellType: T.Type) {
         let className = cellType.className
         let nib = UINib(nibName: className, bundle: nil)
