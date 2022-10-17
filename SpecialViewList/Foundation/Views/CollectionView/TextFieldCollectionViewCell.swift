6123c489ce175845a20ecbf79da6abea345b59f2
yosei <22518469+yosei-yamagishi@users.noreply.github.com>
2022-10-17 14:36:10 +0900

    delegateの追加

--- SpecialViewList/Foundation/Views/CollectionView/TextFieldCollectionViewCell.swift
+++ SpecialViewList/Foundation/Views/CollectionView/TextFieldCollectionViewCell.swift
@@ -1,16 +1,33 @@
 import UIKit
 
-class TextFieldCollectionViewCell: UICollectionViewCell {
+protocol TextFieldCollectionViewCellDelegate: AnyObject {
+    func didBeginEditing(indexPath: IndexPath)
+    func didTapReturn()
+}
 
+class TextFieldCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var textFieldView: UITextField! {
         didSet {
             textFieldView.delegate = self
         }
     }
+    
+    weak var delegate: TextFieldCollectionViewCellDelegate?
+    private var indexPath: IndexPath?
+    
+    func setIndexPath(indexPath: IndexPath) {
+        self.indexPath = indexPath
+    }
 }
 
 extension TextFieldCollectionViewCell: UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        guard let indexPath = indexPath else { return }
+        delegate?.didBeginEditing(indexPath: indexPath)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        delegate?.didTapReturn()
+        return true
     }
 }
